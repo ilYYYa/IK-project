97cabb032d60a240bf2fe986dc9fc981fea4c053
ilYYYa <ilyyya.777@gmail.com>
2017-04-03 21:05:28 +0300

    Commityyy

--- IK_project/src/Window/MainWindow.java
+++ IK_project/src/Window/MainWindow.java
@@ -1,5 +1,7 @@
 package Window;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
@@ -14,5 +16,20 @@ public class MainWindow extends JFrame
 		this.setUndecorated(true);
 		
 		this.setVisible(true);
+		
+		while(true)
+		{
+			repaint();
+		}
+	}
+	
+	int xx = 0;
+	
+	@Override
+	public void paint(Graphics g)
+	{
+		g.setColor(new Color(0,0,0));
+		g.drawRect(xx, 100, 200, 200);
+		xx++;
 	}
 }
