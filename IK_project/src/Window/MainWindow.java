136f00dbabc4e25fbd0293c43375fdeb4f863e08
ilYYYa <ilyyya.777@gmail.com>
2017-04-03 21:09:03 +0300

    Added double buffer

--- IK_project/src/Window/MainWindow.java
+++ IK_project/src/Window/MainWindow.java
@@ -8,6 +8,8 @@ import javax.swing.JFrame;
 
 public class MainWindow extends JFrame
 {
+	DoubleBuffer buff = new DoubleBuffer();
+	
 	public MainWindow()
 	{
 		this.setBounds(0, 0, (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
@@ -15,21 +17,13 @@ public class MainWindow extends JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setUndecorated(true);
 		
+		this.add(buff);
+		
 		this.setVisible(true);
 		
 		while(true)
 		{
 			repaint();
 		}
-	}
-	
-	int xx = 0;
-	
-	@Override
-	public void paint(Graphics g)
-	{
-		g.setColor(new Color(0,0,0));
-		g.drawRect(xx, 100, 200, 200);
-		xx++;
 	}
 }
